1a982ad69bc0e41982422f1f89291490d65efaea
Fergus Lemon <fergus.lemon@gmail.com>
2019-01-13 10:56:43 +0000

    Change React element, play with react-calendar, now showing a basic calendar instead of 'Hello, World'

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,30 @@
 import React, { Component } from 'react';
+import Calendar from 'react-calendar';
 //import logo from './logo.svg';
 //import './App.css';
 
+//class App extends Component {
+//  render() {
+//    return (<h1>Hello, World!</h1>);
+//  }
+//}
+
 class App extends Component {
+  state = {
+    date: new Date(),
+  }
+
+  onChange = date => this.setState({ date })
+
   render() {
-    return (<h1>Hello, World!</h1>);
+    return (
+      <div>
+        <Calendar
+          onChange={this.onChange}
+          value={this.state.date}
+        />
+      </div>
+    );
   }
 }
 
